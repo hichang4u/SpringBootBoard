50539ff1a46efdb1eded403c550e1cc858f35235
woori <woori@DESKTOP-R0KB904>
2024-09-11 17:47:08 +0900

    스프링 부트의 기본 기능 익히기 SBB 서비스 개발하기 - 내비게이션 바 추가 - 페이징 기능 추가 - 게시물에 번호 지정 - 답변 개수 표시 - 스프링 시큐리티 사용 - 회원 가입 기능 구현 - 로그인과 로그아웃 기능 구현 - 글쓴이 항목 추가 - 수정과 삭제 기능 추가

--- SpringBootBoard/src/test/java/com/mysite/sbb/SpringBootBoardApplicationTests.java
+++ SpringBootBoard/src/test/java/com/mysite/sbb/SpringBootBoardApplicationTests.java
@@ -1,13 +1,24 @@
 package com.mysite.sbb;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.question.QuestionService;
 
 @SpringBootTest
 class SpringBootBoardApplicationTests {
 
+	@Autowired
+    private QuestionService questionService;
+
 	@Test
-	void contextLoads() {
-	}
+    void testJpa() {
+		for (int i = 1; i <= 300; i++) {
+            String subject = String.format("제목:테스트 데이터입니다:[%03d]", i);
+            String content = "내용:무[%03d]";
+            this.questionService.create(subject, content, null);
+        }
+    }
 
 }
